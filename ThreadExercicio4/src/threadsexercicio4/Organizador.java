e6fa6412648850237bae307e6a2876597bd55ce0
6569900 <6569900@C170491.cefid.local>
2013-05-16 14:16:37 -0300

    Final

--- ThreadExercicio4/src/threadsexercicio4/Organizador.java
+++ ThreadExercicio4/src/threadsexercicio4/Organizador.java
@@ -6,36 +6,39 @@ import java.util.Collections;
 import java.util.List;
 
 class Organizador {
-   
+
    private int[][] matriz;
-   List<Integer> matrizLista;  
-   
+   List<Integer> matrizLista;
+   int controle;
+
    public Organizador() {
       matriz = new int[3][3];
       matrizLista = new ArrayList<Integer>();
+      this.controle = 0;
    }
-   
+
    public synchronized void organizar(int[] vetor) {
 
-      Arrays.sort(vetor);
-      
       for (int i = 0; i < 3; i++) {
-        matrizLista.add(vetor[i]);
+         matrizLista.add(vetor[i]);
       }
-   }
-   
-   public int[][] getMatrizOrganizada() {
       
-      int i = 0;
-      
-      Collections.sort(matrizLista);
-      
-      for (int linha = 0; linha < 3; linha++) {
-         for (int coluna = 0; coluna < 3; coluna++) {
-            matriz[linha][coluna] = matrizLista.get(i++);
+      controle++;
+
+      if (controle == 3) {
+         int i = 0;
+
+         Collections.sort(matrizLista);
+
+         for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+               matriz[linha][coluna] = matrizLista.get(i++);
+            }
          }
       }
-      
+   }
+
+   public int[][] getMatrizOrganizada() {
       return matriz;
    }
 }
