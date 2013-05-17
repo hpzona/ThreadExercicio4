013344d0c02ba49f4e3576b1d8d7ce8f46baf830
6569900 <6569900@C170491.cefid.local>
2013-05-17 08:30:00 -0300

    Final

--- ThreadExercicio4/src/threadsexercicio4/Classifica.java
+++ ThreadExercicio4/src/threadsexercicio4/Classifica.java
@@ -4,23 +4,25 @@ public class Classifica implements Runnable {
 
    private Organizador org; // objeto compartilhado
    private int linha;
-   int[][] matriz = new int[3][3];
+   int[][] matriz = new int[3][3];;
+   int[] vetor;
 
    Classifica(int linha, int[][] matriz, Organizador org) {
+      vetor = new int[3];
       this.matriz = matriz;
+      this.vetor = vetor;
       this.org = org;
       this.linha = linha;
+      
    }
 
    @Override
    public void run() {
-      int[] vetor = new int[3];
 
-      for (int coluna = 0; coluna < 3; coluna++) {
-         vetor[coluna] = matriz[linha][coluna];
+      for (int i = 0; i < 3; i++) {
+         vetor[i] = matriz[linha][i];
       }
-      
-      org.organizar(vetor);
-   
+
+      org.organizar(linha, vetor);
    }
 }
--- ThreadExercicio4/src/threadsexercicio4/Organizador.java
+++ ThreadExercicio4/src/threadsexercicio4/Organizador.java
@@ -1,39 +1,21 @@
 package threadsexercicio4;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 class Organizador {
 
    private int[][] matriz;
-   List<Integer> matrizLista;
-   int controle;
 
    public Organizador() {
       matriz = new int[3][3];
-      matrizLista = new ArrayList<Integer>();
-      this.controle = 0;
    }
 
-   public synchronized void organizar(int[] vetor) {
+   public synchronized void organizar(int linha, int[] vetor) {
+
+      Arrays.sort(vetor);
 
       for (int i = 0; i < 3; i++) {
-         matrizLista.add(vetor[i]);
-      }
-      
-      controle++;
-
-      if (controle == 3) {
-         int i = 0;
-
-         Collections.sort(matrizLista);
-
-         for (int linha = 0; linha < 3; linha++) {
-            for (int coluna = 0; coluna < 3; coluna++) {
-               matriz[linha][coluna] = matrizLista.get(i++);
-            }
-         }
+         matriz[linha][i] = vetor[i];
       }
    }
 
